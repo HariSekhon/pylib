356266db13afb73fa3aba2c93dabfa57c2041d85
Hari Sekhon <harisekhon@gmail.com>
2015-11-14 12:49:23 +0000

    added test/__init__.py

---
+++ test/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-11-14 12:21:54 +0000 (Sat, 14 Nov 2015)
+#
+#  https://github.com/harisekhon/pytools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+from __future__ import print_function
+
+__author__  = 'Hari Sekhon'
+__version__ = '0.1'
+
+# Stub required for 'python -m unittest discover'
