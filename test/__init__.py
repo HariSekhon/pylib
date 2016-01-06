1ae9202e3f9c2c1ca0a40a4ff1e8e78ca36f0bc9
Hari Sekhon <harisekhon@gmail.com>
2016-01-06 15:46:20 +0000

    pylint rewrite

--- test/__init__.py
+++ test/__init__.py
@@ -8,7 +8,8 @@
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
-#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback
+#  to help improve or steer this or other code I publish
 #
 #  http://www.linkedin.com/in/harisekhon
 #
@@ -18,7 +19,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__author__  = 'Hari Sekhon'
+__author__ = 'Hari Sekhon'
 __version__ = '0.1'
 
 # Stub required for 'python -m unittest discover'
