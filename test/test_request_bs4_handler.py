0c55cbebe8a8842c3e82a80452a4f0a7a0bb83ef
Hari Sekhon <harisekhon@gmail.com>
2016-02-21 22:29:19 +0000

    added test/test_request_bs4_handler.py

---
+++ test/test_request_bs4_handler.py
@@ -0,0 +1,63 @@
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2014-09-15 20:49:22 +0100 (Mon, 15 Sep 2014)
+#
+#  https://github.com/harisekhon/pylib
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback
+#  to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+"""
+# ============================================================================ #
+#                   PyUnit Tests for HariSekhon.RequestBS4Handler
+# ============================================================================ #
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+# from __future__ import unicode_literals
+
+import logging
+import os
+import sys
+import time
+import unittest
+from optparse import OptionConflictError
+# inspect.getfile(inspect.currentframe()) # filename
+import requests
+libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(libdir)
+# pylint: disable=wrong-import-position
+from harisekhon.utils import CodingErrorException, InvalidOptionException, log
+from harisekhon import RequestBS4Handler
+
+
+class RequestBS4HandlerTester(unittest.TestCase):
+
+    # must prefix with test_ in order for the tests to be called
+
+    # Not using assertRaises >= 2.7 and maintaining compatibility with Python 2.6 servers
+
+    # TODO: mock this
+    def test_request_bs4_handler(self):
+        req = RequestBS4Handler.curl('www.google.com')
+        self.assertTrue(isinstance, requests.Response)
+
+
+def main():
+    # increase the verbosity
+    # verbosity Python >= 2.7
+    #unittest.main(verbosity=2)
+    log.setLevel(logging.DEBUG)
+    suite = unittest.TestLoader().loadTestsFromTestCase(RequestBS4HandlerTester)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+if __name__ == '__main__':
+    main()
