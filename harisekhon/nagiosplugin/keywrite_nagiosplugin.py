41f287ce467c7e4d7c4b2a3d107e9cb1b1aa2053
Hari Sekhon <harisekhon@gmail.com>
2016-03-20 22:12:00 +0000

    updated harisekhon/nagiosplugin/keywrite_nagiosplugin.py

--- harisekhon/nagiosplugin/keywrite_nagiosplugin.py
+++ harisekhon/nagiosplugin/keywrite_nagiosplugin.py
@@ -4,7 +4,7 @@
 #  Author: Hari Sekhon
 #  Date: 2016-02-14 16:25:48 +0000 (Sun, 14 Feb 2016)
 #
-#  https://github.com/harisekhon/pytools
+#  https://github.com/harisekhon/pylib
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
