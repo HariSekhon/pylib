c8ef1633204dc237e904c7c91e21dda84b4c4b7e
Hari Sekhon <harisekhon@gmail.com>
2016-06-14 13:00:09 +0100

    updated test/test_utils.py

--- test/test_utils.py
+++ test/test_utils.py
@@ -1146,6 +1146,8 @@ class UtilsTester(unittest.TestCase):
         self.assertEqual(plural(0), 's')
         self.assertEqual(plural('0'), 's')
         self.assertEqual(plural(2), 's')
+        self.assertEqual(plural(3.0), 's')
+        self.assertEqual(plural("3.0"), 's')
 
     def test_random_alnum(self):
         self.assertTrue(re.match(r'\w{20}', random_alnum(20)))
