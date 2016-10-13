096d8594e085a324304465581b955a8a7f2be1eb
Hari Sekhon <harisekhon@gmail.com>
2016-10-13 10:32:11 +0100

    added harisekhon/hbase/__init__.py

---
+++ harisekhon/hbase/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2016-10-13 09:34:48 +0100 (Thu, 13 Oct 2016)
+#
+#  https://github.com/harisekhon/nagios-plugins
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+# Stub
