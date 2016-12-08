c29c741aae03861fe62f7964597c89b2582a579c
Hari Sekhon <harisekhon@gmail.com>
2016-12-08 17:46:36 +0000

    updated __main__.py

--- test/__main__.py
+++ test/__main__.py
@@ -8,7 +8,8 @@
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
-#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help improve or steer this or other code I publish
 #
 #  http://www.linkedin.com/in/harisekhon
 #
@@ -18,14 +19,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__author__  = 'Hari Sekhon'
-__version__ = '0.1'
-
-import glob
-import inspect
 import os
-import subprocess
-import sys
+#import glob
+#import inspect
+#import subprocess
+#import sys
 ## using optparse rather than argparse for servers still on Python 2.6
 #from optparse import OptionParser
 # libdir = os.path.join(os.path.dirname(inspect.getfile(inspect.currentframe())), '..')
@@ -36,6 +34,9 @@ libdir = os.path.join(os.path.dirname(__file__), '..')
 # except ImportError, e:
 #    print('module import failed: %s' % e)
 #    sys.exit(4)
+
+__author__ = 'Hari Sekhon'
+__version__ = '0.1'
 
 def main():
     print('running unit tests')
