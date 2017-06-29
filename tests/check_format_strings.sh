376422e971f90a7482b1db6d3a64cd587fb3b87e
Hari Sekhon <harisekhon@gmail.com>
2017-06-29 15:56:42 +0200

    added tests/check_format_strings.sh

---
+++ tests/check_format_strings.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2017-06-29 15:48:48 +0200 (Thu, 29 Jun 2017)
+#
+#  https://github.com/harisekhon/pylib
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+set -u
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$srcdir/..";
+
+. ./tests/utils.sh
+
+set +e
+for x in ${@:-$(find . -iname '*.py' -o -iname '*.jy')}; do
+    isExcluded "$x" && continue
+    output="$(egrep ' log\.(info|warn|error|debug|notice)' "$x" | grep -v "['\"]")"
+    if [ -n "$output" ]; then
+        echo "$x contains potentially unsafe string interpolation behaviour:"
+        echo "$output"
+        echo
+    fi
+done
+
+exit 0
