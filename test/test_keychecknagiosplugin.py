a11dce75b88a63570e516b6a7a887711b663e73f
Hari Sekhon <harisekhon@gmail.com>
2017-08-12 09:03:25 +0100

    updated test_keychecknagiosplugin.py

--- test/test_keychecknagiosplugin.py
+++ test/test_keychecknagiosplugin.py
@@ -34,6 +34,7 @@ sys.path.append(libdir)
 from harisekhon.utils import log
 from harisekhon import KeyCheckNagiosPlugin
 
+
 class KeyCheckNagiosPluginTester(unittest.TestCase):
 
     # must prefix with test_ in order for the tests to be called
@@ -72,6 +73,7 @@ def main():
     log.setLevel(logging.DEBUG)
     suite = unittest.TestLoader().loadTestsFromTestCase(KeyCheckNagiosPluginTester)
     unittest.TextTestRunner(verbosity=2).run(suite)
+
 
 if __name__ == '__main__':
     main()
