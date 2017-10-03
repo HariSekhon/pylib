a2e598303012dad0795c75dd949a5e3e8f5a169d
Hari Sekhon <harisekhon@gmail.com>
2017-10-03 14:05:47 +0200

    fixed typo

--- harisekhon/nagiosplugin/rest_nagiosplugin.py
+++ harisekhon/nagiosplugin/rest_nagiosplugin.py
@@ -127,7 +127,7 @@ class RestNagiosPlugin(NagiosPlugin):
             url += self.path.lstrip('/')
         auth = None
         if self.user and self.password:
-            log.info('authenicating to rest API')
+            log.info('authenticating to rest API')
             auth = (self.user, self.password)
         req = self.request.get(url, auth=auth)
         return req
