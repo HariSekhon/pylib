3c80c9fde29b84bdd1d3ce2d5ae2f9437a5da47f
Hari Sekhon <harisekhon@gmail.com>
2019-02-18 22:04:27 +0000

    updated test/test_utils.py

--- test/test_utils.py
+++ test/test_utils.py
@@ -174,7 +174,7 @@ class UtilsTester(unittest.TestCase):
 
     def test_get_topfile(self):
         topfile = get_topfile()
-        self.assertTrue(re.search(r'\b(unit2|nosetests|py.?test(?:-2)?|coverage|\.py)$', topfile))
+        self.assertTrue(re.search(r'\b(unit2|nosetests(?:-\d+(?:\.\d+)?)?|py.?test(?:-\d+(?:\.\d+)?)?|coverage|\.py)$', topfile))
         # comes out as utrunner.py in IDE or python2.7/runpy.py
         # self.assertEqual('test_utils.py', get_topfile())
 
