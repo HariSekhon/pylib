9d0ec2a83e33d137efdadb68213bf1b0a2ed12f5
Hari Sekhon <harisekhon@gmail.com>
2019-03-14 19:53:33 +0000

    updated test/test_utils.py

--- test/test_utils.py
+++ test/test_utils.py
@@ -897,6 +897,7 @@ class UtilsTester(unittest.TestCase):
 
     def test_isLdapDn(self):
         self.assertTrue(isLdapDn('uid=hari,cn=users,cn=accounts,dc=local'))
+        self.assertTrue(isLdapDn('cn=hari-superuser,cn=big-users,cn=accounts,dc=local'))
         self.assertFalse(isLdapDn('hari@LOCAL'))
         self.assertFalse(isLdapDn(' '))
         self.assertFalse(isLdapDn(None))
@@ -2693,6 +2694,7 @@ class UtilsTester(unittest.TestCase):
     def test_validate_ldap_dn(self):
         self.assertTrue(validate_ldap_dn('uid=hari,cn=users,cn=accounts,dc=local'))
         self.assertTrue(validate_ldap_dn('uid=hari,cn=users,cn=accounts,dc=local', 'name'))
+        self.assertTrue(validate_ldap_dn('cn=hari-superuser,cn=big-users,cn=accounts,dc=local'))
 
     def test_validate_ldap_dn_exception(self):
         try:
