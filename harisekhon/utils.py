a4f71b0f12ebb55deba0c085ad8f90fd53349f8d
Hari Sekhon <harisekhon@gmail.com>
2020-01-08 15:15:00 +0000

    improved jsonpp() to be able to dump datetime objects

--- harisekhon/utils.py
+++ harisekhon/utils.py
@@ -647,7 +647,8 @@ def read_file_without_comments(filename):
 def jsonpp(json_data):
     if isStr(json_data):
         json_data = json.loads(json_data, encoding='utf-8')
-    return json.dumps(json_data, sort_keys=True, indent=4, separators=(',', ': '))
+    # default=str converts otherwise unconvertible types like datetime objects to be dumpable to strings
+    return json.dumps(json_data, sort_keys=True, indent=4, separators=(',', ': '), default=str)
 
 
 def list_sort_dicts_by_value(my_list, key):
