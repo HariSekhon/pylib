c690c6df414521f7a1a907a07f0f9f38d470b53a
Hari Sekhon <harisekhon@gmail.com>
2020-03-25 14:50:03 +0000

    updated coverage.py

--- test/coverage.py
+++ test/coverage.py
@@ -19,7 +19,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__author__  = 'Hari Sekhon'
+__author__ = 'Hari Sekhon'
 __version__ = '0.1'
 
 import os
